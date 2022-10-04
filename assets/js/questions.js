d49862da5c399f7f409ffd661c166e74224835db
GraeA85 <graemearthur36@gmail.com>
2022-10-04 15:31:56 +0000

    initial commit, add all pages, sounds, images and css - create basic layout on pages and create initial JS variables and functions

---
+++ assets/js/questions.js
@@ -0,0 +1,77 @@
+let quizQuestions = [
+    {
+    question: "What is Comic Book Guy's real name?",
+    answers:["Clark Thompson", "Herschel Krustowsky", "Jeffrey Albertson", "Tom McNeil"],
+    corectAnswer: "Jeffrey Albertson"
+    },
+    {
+    question:"What was the name of Grampa's military unit in World War 2?",
+    answers: ["The Flying Hellfish", "The Sailing Sailfish", "The Tussle Tanks", "The Subpar Subbers"],
+    corectAnswer: "The Flying Hellfish",
+    },
+    {
+    question: "What is the password Mr Burns uses to enter his panic room? (hint, it's also his place of birth!)",
+    answers: ["Nantucket", "Shelbyville", "Pangea", "Ogdenville"],
+    corectAnswer: "Pangea",
+    },
+    {
+    question: "What is the Crazy Cat Lady's real name?",
+    answers: ["Susan Diamond", "Francine Cauldron", "Gertrude Fronch", "Eleanor Abernathy"],
+    corectAnswer: "Eleanor Abernathy"
+    },
+    {
+    question: "What is Groundskeeper Willies Surname?",
+    answers: ["McArthur", "MacDonald", "McKenzie", "MacDougal"],
+    corectAnswer: "MacDougal",
+    },
+    {
+    question: "What is the name of the university Moe attended?",
+    answers: ["Drinkston University", "Swigmore University", "Chugsville University", "He didn't attend University"],
+    corectAnswer: "Swigmore University",
+    },
+    {
+    question: "What is the pet name of the car driven by the bandit Snake?",
+    answers: ["Red Rooster", "King Cobra", "Canyonero", "Lil' Bandit"],
+    corectAnswer: "Lil' Bandit"
+    },
+    {
+    question: "What job does Krusty the Clowns dad do?",
+    answers: ["Carpenter", "Rabbi", "Organ shop owner", "Car salesman"],
+    corectAnswer: "Rabbi"
+    },
+    {
+    question: "Who is the head of the Springfield Historical Society?",
+    answers: ["Hollis Hurlbut", "Colin Chowder", "Professor Frink", "Chester Springfield"],
+    corectAnswer: "Hollis Hurlbut"
+    },
+    {
+    question: "What was the brand of TV Homer had when he was a child?",
+    answers: ["The Panaphonic", "Carnivale", "The Radiation King", "Magnetbox"],
+    corectAnswer: "The Radiation King"
+    },
+    {
+    question: "What is the name of the doll that Lisa creates as an alternative to Malibu Stacy?",
+    answers: ["Sally Slippers", "Aruba Alice", "Margie", "Lisa Lionheart"],
+    corectAnswer: "Lisa Lionheart"
+    },
+    {
+    question: "What job did Ned Flanders have before he owned the Leftorium?",
+    answers: ["Gardener", "Barber", "Pharmaceutical Representative", "Church organist"],
+    corectAnswer: "Pharmaceutical Representative"
+    },
+    {
+    question: "How did Poochie die?",
+    answers: ["He accidently ate chocolate", "Itchy and Scratchy disembowled him", " He was ran over by a steamroller", "He died on the way back to his home planet"],
+    corectAnswer: "He died on the way back to his home planet"
+    },
+    {
+    question: "What was the name of the internet company that Homer set up?",
+    answers: ["Compu-Global-Hyper-Mega-Net", "Homer's Interwebs Service", "Simpson's Slimline Internet Services", "Micro-Worldwide-Super-Massive-Web"],
+    corectAnswer: "Compu-Global-Hyper-Mega-Net"
+    },
+    {
+    question: "What is the name of the town that the Simpsons move to when Homer gets a new job at the Globex Corporation?",
+    answers: ["Frigid Falls", "Swartzwelder County", "Cypress Creek", "Springshire"],
+    corectAnswer: "Cypress Creek"
+    }
+];
