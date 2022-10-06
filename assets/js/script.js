804e5b95594a6bcfcf23c1ad12a35232534fc2ba
GraeA85 <graemearthur36@gmail.com>
2022-10-06 21:38:15 +0000

    start build of quiz, quiz shown on game page, linked js files in html page

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,7 +2,34 @@ const quizContainer = document.getElementById('quizgame');
 const resultsContainer = document.getElementById('results');
 const submitButton = document.getElementById('submit');
 
-function buildQuiz(){}
+
+
+function buildQuiz(){
+const output = [];
+
+quizQuestions.forEach( (currentQuestion, questionNumber) => {
+
+   // variable to store list of answers 
+   const answers = []; 
+
+   for(letter in currentQuestion.answers){
+
+    answers.push(
+        `<label>
+        <input type="radio" name="question${questionNumber}" value = ${letter}> : ${currentQuestion.answers[letter]}
+        </label>`
+    );
+   }
+
+   output.push(
+    `<div class="question"> ${currentQuestion.question}</div>
+    <div class="answers"> ${answers.join('')} </div>`
+   );
+}
+
+);
+quizContainer.innerHTML = output.join('');
+}
 
 function showResults(){}
 
